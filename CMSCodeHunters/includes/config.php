bf8f725cbb890c1e17c5043144ea9792c1b99762
ahmedash95 <ahmed29329@gmail.com>
2015-04-22 06:43:57 +0200

    Define paths and Seperators

--- CMSCodeHunters/includes/config.php
+++ CMSCodeHunters/includes/config.php
@@ -5,3 +5,13 @@ define("DB_HOST", "localhost");
 define("DB_USER", "root");
 define("DB_PASS", "");
 define("DB_NAME", "cms");
+
+// Directory Separator ( Windows => '\' , Linux => '/')
+define('DS', DIRECTORY_SEPARATOR);
+define('PS', PATH_SEPARATOR);
+
+define('CLASSES_PATH','classes'.DS);
+define('CONTROLLER_PATH','controller'.DS);
+define('INCLUDES_PATH','includes'.DS);
+
+
